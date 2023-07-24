a4650250c8d01692941af4cd4358754f9d04175f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-07-24 12:39:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/goit-final-project">
+          <BrowserRouter basename="/react_node_fs_66_serhii-vasylenko">
             <App />
             <Toaster />
           </BrowserRouter>
